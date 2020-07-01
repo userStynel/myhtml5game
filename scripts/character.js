4cd59eee999526f2af21c155db2712d7b7b8462d
Stynel <ltnyst10@gmail.com>
2020-07-01 11:00:20 +0900

    I

---
+++ scripts/character.js
@@ -0,0 +1,48 @@
+function LoadImages(scene)
+{
+	scene.load.spritesheet('rabbit', './assets/rabbit.png',  { frameWidth: 128, frameHeight: 128});
+    scene.load.spritesheet('idle_pic', './assets/as.png', { frameWidth: 128, frameHeight: 128});
+    scene.load.spritesheet('attacking_pic', './assets/as_attacking.png', {frameWidth: 128, frameHeight: 128});
+    scene.load.spritesheet('character2', './assets/character2.png', {frameWidth: 128, frameHeight: 128});
+    scene.load.image('shuriken', './assets/shuriken.png');
+}
+
+function LoadAnimation(scene)
+{
+	 scene.anims.create({
+        key: 'idle_left',
+        frames: scene.anims.generateFrameNumbers('idle_pic', { start: 0, end: 4 }),
+        frameRate: 10,
+        repeat: -1
+        });
+    scene.anims.create({
+        key: 'idle_right',
+        frames: scene.anims.generateFrameNumbers('idle_pic', { start: 5, end: 9}),
+        frameRate: 10,
+        repeat: -1
+        });
+    scene.anims.create({
+        key: 'attack_left',
+        frames: scene.anims.generateFrameNumbers('attacking_pic', { start: 0, end: 1 }),
+        frameRate: 5,
+        repeat: 0
+        });
+    scene.anims.create({
+        key: 'attack_right',
+        frames: scene.anims.generateFrameNumbers('attacking_pic', { start: 2, end: 3 }),
+        frameRate: 5,
+        repeat: 0
+        });
+    scene.anims.create({
+        key: 'idle2_left',
+        frames: scene.anims.generateFrameNumbers('character2', { start: 0, end: 4 }),
+        frameRate: 5,
+        repeat: -1
+        });
+    scene.anims.create({
+        key: 'rabbit_idle',
+        frames: scene.anims.generateFrameNumbers('rabbit', { start: 0, end: 4 }),
+        frameRate: 5,
+        repeat: -1
+        });
+}
---
+++ scripts/statemachine.js
@@ -0,0 +1,104 @@
+class StateMachine
+    {
+        constructor(initialState, possibleStates, stateArgs=[])
+        {  
+            this.initialState = initialState;
+            this.possibleStates = possibleStates;
+            this.stateArgs = stateArgs;
+            this.state = null;
+            for(const state of Object.values(this.possibleStates))
+            {
+                state.stateMachine = this;
+            }
+        }
+
+        step()
+        {
+            if(this.state == null)
+            {
+                this.state = this.initialState;
+                this.possibleStates[this.state].enter(this.stateArgs[0], this.stateArgs[1]);
+            }
+            this.possibleStates[this.state].execute(this.stateArgs[0], this.stateArgs[1]);
+        }
+
+        transistion(newState)
+        {  
+            this.state = newState;
+            this.possibleStates[this.state].enter(this.stateArgs[0], this.stateArgs[1]);
+        }
+
+    }
+    class State
+    {
+        enter(){}
+        execute(){}
+    }
+
+    class IdleState extends State
+    {
+        enter(scene, hero)
+        {
+            hero.setVelocity(0);
+        }
+        execute(scene, hero)
+        {
+            if (scene.keys.A.isDown || scene.keys.D.isDown || scene.keys.W.isDown || scene.keys.S.isDown)
+            {
+                this.stateMachine.transistion('move');
+                return;
+            }
+            if(scene.keys.SPACE.isDown)
+            {
+                this.stateMachine.transistion('swing');
+                return;
+            }
+            hero.anims.play('idle_'+hero.direction, true);
+        }
+    }
+
+    class MoveState extends State
+    {
+        execute(scene, hero)
+        {
+            if(scene.keys.SPACE.isDown)
+            {
+                this.stateMachine.transistion('swing');
+                return;
+            }
+            if(!scene.keys.A.isDown && !scene.keys.D.isDown && !scene.keys.W.isDown && !scene.keys.S.isDown)
+            {
+                this.stateMachine.transistion('idle');
+                return;
+            }
+            if(scene.keys.A.isDown)
+            {
+                hero.setVelocityX(-100);
+                hero.direction = "left"
+            }
+            if(scene.keys.D.isDown)
+            {
+                hero.setVelocityX(100);
+                hero.direction = "right"
+            }
+            if(scene.keys.W.isDown)
+            {
+                hero.setVelocityY(-300);
+            }
+            if(scene.keys.S.isDown)
+            {
+                hero.setVelocityY(300);
+            }
+            hero.anims.play('idle_'+hero.direction, true);
+        }
+    }
+
+    class SwingState extends State
+    {
+        enter(scene, hero)
+        {
+            hero.setVelocityX(0);
+            hero.anims.play('attack_'+hero.direction);
+            hero.once('animationcomplete', () => {this.stateMachine.transistion('idle');});
+        }
+    }
