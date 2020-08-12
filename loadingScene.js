ab8c55b33e8cddc9c31e853c64c9f15d8abfb4ca
Stynel <ltnyst10@gmail.com>
2020-08-12 12:30:26 +0900

    ㅇㅇ
    ㅇㅇ

--- loadingScene.js
+++ loadingScene.js
@@ -7,12 +7,9 @@ class LoadingScene extends Phaser.Scene
     preload()
     {
         this.load.image('logo', './assets/UI/seismic_production.png');
-        this.load.image('af', './assets/UI/af.png');
     }
     create()
     {
-        this.add.image(360, 450, 'af');
-        this.add.text(255, 550,'2019-07-09 ~ 2021-04-28');
         this.add.image(660, 250, 'logo');
         var startTXT = this.add.text(660, 400, 'START!');
         startTXT.setInteractive();
